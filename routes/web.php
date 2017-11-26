77729f19c5ecd9741417e7d34ef09a682c00ce1c
Miroslav Vitanov <mvvitanov@gmail.com>
2017-11-26 14:50:21 +0200

    Setup Dashboard as main view and add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::get('/', [
+        'uses' => 'DashboardController@view',
+        'as'   => 'dashboard',
+    ]);
+    
+});
