1a95e8840993028d1d002571a236c9231cc9e003
Miroslav Vitanov <mvvitanov@gmail.com>
2017-12-16 17:28:26 +0200

    Change filters view

--- resources/views/vendor/administr/filters/filters.blade.php
+++ resources/views/vendor/administr/filters/filters.blade.php
@@ -1,10 +1,21 @@
-<div class="administr-filters">
+<div style="padding: 20px;">
     <form>
-        {!! $builder->render() !!}
-
-        {!! $filterBtn->render() !!}
-        <a href="{{ $clearBtn['url'] }}">
-            {{ $clearBtn['label'] }}
-        </a>
+        @foreach(collect($builder->fields())->chunk(3) as $fields)
+            <div class="row">
+                @foreach($fields as $field)
+                    <div class="col-sm-4">
+                        {!! $field->render() !!}
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
+        <div class="row">
+            <div class="col-sm-12">
+                {!! $filterBtn->render() !!}
+                <a href="{{ $clearBtn['url'] }}" class="btn btn-default">
+                    {{ $clearBtn['label'] }}
+                </a>
+            </div>
+        </div>
     </form>
 </div>
