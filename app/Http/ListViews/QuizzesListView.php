2003362c3f9fc0d8f027e19aae4daf61979a4083
Miroslav Vitanov <mvvitanov@gmail.com>
2017-12-16 17:28:16 +0200

    Add filters

--- app/Http/ListViews/QuizzesListView.php
+++ app/Http/ListViews/QuizzesListView.php
@@ -21,27 +21,30 @@ class QuizzesListView extends ListView
             ->text('ends_at', 'End date', [
                 'sortable' => true,
             ])
-            ->text('is_active', 'Is active', function(Column $column) {
+            ->text('is_active', 'Is active', function (Column $column) {
                 $column->format('castBool:is_active', 'yesno');
             })
-            ->actions('', function(Actions $actions) {
+            ->actions('', function (Actions $actions) {
                 $actions
                     ->action('add', 'Create')
                     ->icon('fa fa-plus')
-                    ->url( route('quizzes.create') )
+                    ->url(route('quizzes.create'))
                     ->setGlobal();
 
                 $actions
                     ->action('edit', '')
                     ->icon('fa fa-edit')
-                    ->define(function(Action $action, array $row){
-                        $action->url( route('quizzes.edit', [$row['id']]) );
+                    ->define(function (Action $action, array $row) {
+                        $action->url(route('quizzes.edit', [$row['id']]));
                     });
-            })
-            ;
+            });
     }
 
     protected function filters(Filters $filter)
     {
+        $filter
+            ->text('name', 'Quiz name')
+            ->datetime('starts_at', 'Starts at')
+            ->datetime('ends_at', 'Ends at');
     }
 }
--- app/QueryFilters/QuizzesFilter.php
+++ app/QueryFilters/QuizzesFilter.php
@@ -4,8 +4,29 @@ namespace App\QueryFilters;
 
 use Administr\QueryFilters\Filter;
 use App\Http\ListViews\QuizzesListView;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 
 class QuizzesFilter extends Filter
 {
+    public function name($value)
+    {
+        return $this->builder->where('name', 'like', "%{$value}%");
+    }
+
+    public function startsAt($value)
+    {
+        return $this->builder->whereBetween('starts_at', [
+            Carbon::parse($value)->startOfDay(),
+            Carbon::parse($value)->endOfDay(),
+        ]);
+    }
+
+    public function endsAt($value)
+    {
+        return $this->builder->whereBetween('ends_at', [
+            Carbon::parse($value)->startOfDay(),
+            Carbon::parse($value)->endOfDay(),
+        ]);
+    }
 }
