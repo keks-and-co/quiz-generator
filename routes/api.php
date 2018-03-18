012ad608c381586a432a39c2ee5a6eb41c59bc0a
Miroslav Vitanov <mvvitanov@gmail.com>
2018-03-18 13:06:36 +0200

    Add placeholder route to receive responses

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Models\Quiz;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/quiz/{id}', function($id) {
+Route::get('/quiz/{id}', function ($id) {
     $quiz = fractal()
         ->item(
             Quiz::with('questions.answers', 'questions.type')
@@ -30,13 +31,26 @@ Route::get('/quiz/{id}', function($id) {
 
     $questionsCount = count($quiz['questions']);
 
-    if($questionsCount <= 0) {
+    if ($questionsCount <= 0) {
         $questionsCount = 1;
     }
 
     $chunkSize = round($questionsCount / $quiz['per_page']);
 
     $quiz['questions'] = array_chunk($quiz['questions'], $chunkSize);
-    
+
     return $quiz;
+});
+
+Route::post('/quiz/{id}', function ($id) {
+    $quiz = Quiz::find($id);
+
+    if (!$quiz) {
+        return response([
+            'status'  => 'error',
+            'message' => 'quiz-not-found',
+        ], Response::HTTP_NOT_FOUND);
+    }
+
+    return response(['status' => 'success', 'message' => 'quiz-response-saved']);
 });
